54b2d326835f63ce42982a1d3901fcf50e7e5c26
Cody Wages <92700624+CodyWages@users.noreply.github.com>
2021-12-19 21:22:48 -0600

    Update api.py

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:cw@localhost/devl
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-#User Class/Model
+#Developer Class/Model
 class Developer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
