ace910800cf40aa3fa8f2a554e11ce34bd44a188
Cody Wages <92700624+CodyWages@users.noreply.github.com>
2022-02-04 21:18:16 -0600

    Update api.py

--- api.py
+++ api.py
@@ -2,16 +2,16 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import logging
 
-#Logging 
+# Logging 
 logging.basicConfig(filename='dd.log', level=logging.INFO, format='%(levelname)s:%(message)s')
 
-#Init App
+# App Setup
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:cw@localhost/devlist'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-#Developer Class/Model
+# Developer Class/Model
 class Developer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
@@ -22,7 +22,7 @@ class Developer(db.Model):
         self.screenname = screenname
         logging.info('Created Developer: {} - {}'.format(self.username, self.screenname))
         
-#POST New Dev Info
+# POST New Dev Info
 @app.route('/devs', methods=['POST'])
 def add_devs():
     username = request.json['username']
@@ -32,7 +32,7 @@ def add_devs():
     db.session.commit()    
     return {"username": new_dev.username, "screenname": new_dev.screenname}
 
-#GET All Devs Info
+# GET All Devs Info
 @app.route('/devs')
 def get_devs():
     devs = Developer.query.all()
@@ -42,12 +42,12 @@ def get_devs():
         output.append(dev_data)
     return {"devs": output}
 
-#GET Specific Dev Info
+# GET Specific Dev Info
 @app.route('/devs/<id>')
 def get_dev(id):
     dev = Developer.query.get_or_404(id)
     return {"username": dev.username, "screenname": dev.screenname}
 
-#Run Server
+# Run Server
 if __name__ == '__main__':
     app.run(debug=True)
